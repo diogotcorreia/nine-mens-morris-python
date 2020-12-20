3acd6f766c555afaa72f0313b41cc30f48a2ee0f
Diogo Correia <me@diogotc.com>
2020-12-20 22:50:24 +0000

    Add TAD posição
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ nine-mens-morris.py
@@ -0,0 +1,43 @@
+# Diogo Torres Correia - ist199211
+
+###############
+# TAD Posicao #
+###############
+
+def cria_posicao(c, l):
+    if type(c) != str or type(l) != int or c not in 'abc' or not 1 <= l <= 3:
+        raise ValueError("cria_posicao: argumentos invalidos")
+    return {'c': c, 'l': l}
+
+
+def cria_copia_posicao(pos):
+    return {'c': pos['c'], 'l': pos['l']}
+
+
+def obter_pos_c(pos):
+    return pos['c']
+
+
+def obter_pos_l(pos):
+    return pos['l']
+
+
+def eh_posicao(pos):
+    return type(pos) == dict and \
+        'c' in pos and \
+        'l' in pos and \
+        type(pos['c']) != str or \
+        type(pos['l']) != int or \
+        pos['c'] not in 'abc' or \
+        not 1 <= pos['l'] <= 3
+
+
+def posicoes_iguais(p1, p2):
+    return eh_posicao(p1) and \
+        eh_posicao(p2) and \
+        obter_pos_c(p1) == obter_pos_c(p2) and \
+        obter_pos_l(p1) == obter_pos_l(p2)
+
+
+def posicao_para_str(pos):
+    return obter_pos_c(pos) + obter_pos_l(pos)
