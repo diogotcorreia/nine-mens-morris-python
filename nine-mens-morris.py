d062fce8628fe54c8adf6a571214a70d0e09ba49
Diogo Correia <me@diogotc.com>
2020-12-21 13:45:09 +0000

    Add TAD peça
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- nine-mens-morris.py
+++ nine-mens-morris.py
@@ -105,3 +105,68 @@ def obter_posicoes_adjacentes(pos):
 
     return tuple(sorted(resultado,
                         key=lambda x: obter_pos_l(x) + obter_pos_c(x)))
+
+############
+# TAD Peca #
+############
+
+
+def cria_peca(peca):
+    """
+    cria_peca: str -> peca
+    Recebe um identificador de jogador (ou peca livre) e
+    devolve um dicionario que corresponde ah representacao
+    interna da peca.
+    R[peca] -> {'peca': peca}
+    """
+    if type(peca) != str or len(peca) != 1 or peca not in 'XO ':
+        raise ValueError('cria_peca: argumento invalido')
+    return {'peca': peca}
+
+
+def cria_copia_peca(peca):
+    """
+    cria_copia_peca: peca -> peca
+    Recebe uma peca e devolve uma copia nova da peca
+    """
+    return {'peca': peca['peca']}
+
+
+def eh_peca(arg):
+    """
+    eh_peca: universal -> booleano
+    Devolve True caso o seu argumento seja um TAD peca
+    ou False em caso contrario.
+    """
+    return type(arg) == dict and \
+        len(arg) == 1 and \
+        'peca' in arg and \
+        type(arg['peca']) == str and \
+        len(arg['peca']) == 1 and \
+        arg['peca'] in 'XO '
+
+
+def pecas_iguais(p1, p2):
+    """
+    pecas_iguais: peca X peca -> booleano
+    Devolve True apenas se os argumentos dados sao pecas e
+    sao iguais. Caso contrario, devolve False.
+    """
+    return eh_peca(p1) and eh_peca(p2) and p1['peca'] == p2['peca']
+
+
+def peca_para_str(peca):
+    """
+    peca_para_str: peca -> str
+    Devolve a cadeira de caracteres que representa o jogador dono
+    da peca.
+    """
+    return '[{}]'.format(peca['peca'])
+
+
+def peca_para_inteiro(peca):
+    """
+    peca_para_inteiro: peca -> N
+    Devolve um inteiro -1, 1 ou 0, de acordo com o jogador dono da peca.
+    """
+    return {'[X]': 1, '[O]': -1, '[ ]': 0}[peca_para_str(peca)]
